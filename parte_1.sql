cabd1081b48c0a876f0af84d561161a0787f672a
ivan <ivan.rowlands.macedo@gmail.com>
2022-02-07 09:48:10 -0300

    feature: part 1 finish

---
+++ parte_1.sql
@@ -0,0 +1,17 @@
+create database kopa_db;
+
+create table kopas(
+	id 	BIGSERIAL CONSTRAINT pk_kopas primary key,
+	selecao VARCHAR(128) unique,
+	qnt_copas INTEGER
+);
+
+insert into kopa(selecao, qnt_copas) values ('Brasil', 5), ('Alemanha', 5), ('Itália', 4), ('Argentina', 2), ('França', 2), ('Uruguai', 1), ('Espanha', 1), ('Japão', 1);
+
+select selecao, qnt_copas from kopas;
+
+update kopas set qnt_copas = 4 where selecao = 'Alemanha';
+
+delete from kopas where selecao = 'Japão';
+
+select * from kopas;
